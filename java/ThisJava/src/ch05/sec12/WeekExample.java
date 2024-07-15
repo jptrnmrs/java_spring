479fcbcdd3675185c99d725a5cf16b36f0a5bbfd
under912 <under912@naver.com>
2024-07-15 17:13:07 +0900

    7월 15일 (월) 수업내용

--- java/ThisJava/src/ch05/sec12/WeekExample.java
+++ java/ThisJava/src/ch05/sec12/WeekExample.java
@@ -13,20 +13,21 @@ public class WeekExample {
 		//Calendar 얻기
 		Calendar cal = Calendar.getInstance();
 		
-		
+		//오늘의 요일 얻기(1~7)
 		int week = cal.get(Calendar.DAY_OF_WEEK);
 		
+		//숫자를 열거 상수로 변환해서 변수에 대입
 		switch(week) {
-			case 1: today = Week.SUNDAY; break;
-			case 2: today = Week.MONDAY; break;
-			case 3: today = Week.TUESDAY; break;
-			case 4: today = Week.WEDNESDAY; break;
-			case 5: today = Week.THURSDAY; break;
-			case 6: today = Week.FRIDAY; break;
-			case 7: today = Week.SATURDAY; break;				
+			case 1: today = Week.SUNDAY; 	break;
+			case 2: today = Week.MONDAY; 	break;
+			case 3: today = Week.TUESDAY; 	break;
+			case 4: today = Week.WEDNESDAY;	break;
+			case 5: today = Week.THURSDAY;	break;
+			case 6: today = Week.FRIDAY;	break;
+			case 7: today = Week.SATURDAY;	break;				
 		}
 		
-		//열거값을 비교
+		//열거 타입 변수를 사용
 		if(today == Week.FRIDAY) {
 			System.out.println("오늘은 신나는 금요일 입니다.");
 		}	else { 
